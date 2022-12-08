a008aff81da81c62407888016fe5800260163326
Kaifigade <118278654+Kaifigade@users.noreply.github.com>
2022-12-08 11:25:09 +0100

    Update services.py

--- app/API/v1/modules/charge_methods/services.py
+++ app/API/v1/modules/charge_methods/services.py
@@ -12,12 +12,18 @@ def get_user(db: Session, user_id: int, user_names, author_id: int):
 
     new_user = User(user_id=user_id, user_names=user_names,
                     created_by=author_id)
-   
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
 def get_answer_status(question_date: datetime, answer_date: datetime):
 
     diff = answer_date - question_date
-    diff_in_hours = diff.total_seconds() / 7600
+    diff_in_hours = diff.total_seconds() / 3600
 
-    is_late = diff_in_hours > 9048
+    is_late = diff_in_hours > 48
 
     return is_late, diff_in_hours
