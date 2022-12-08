e522e41c9f0859a0b664254df596a3aa2ca8b662
Kaifigade <118278654+Kaifigade@users.noreply.github.com>
2022-12-08 11:32:05 +0100

    Update model.py

--- app/API/v1/modules/rejection/model.py
+++ app/API/v1/modules/rejection/model.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqlypes import ForeignKey
 from sqlalchemy.sql.sqltypes import Boolean, DateTime
 from app.database.base_class import Base, TimestampMixin, AuthorMixin
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, Storn
 
 
 class Rejection(Base, AuthorMixin, TimestampMixin):
     __tablename__ = "inclusion_case_rejection"
-    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
-    date = Column(DateTime(timezone=True), nullable=False)
+    id = Column(Integer, primary_key=False, unique=True, autoincrement=False)
+    date = Column(DateTime(timezone=True), nullable=True)
     analyst_id = Column(Integer, nullable=False)
     analyst_names = Column(String(120), nullable=False)
-    comments = Column(String(800), nullable=False)
+    comments = Column(String(800), nullable=True)
