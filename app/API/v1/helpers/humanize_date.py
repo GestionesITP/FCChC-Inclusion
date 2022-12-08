a330e4d33c901ae46996ca7a28804afe77d5654d
Kaifigade <118278654+Kaifigade@users.noreply.github.com>
2022-12-08 11:50:24 +0100

    Update humanize_date.py

--- app/API/v1/helpers/humanize_date.py
+++ app/API/v1/helpers/humanize_date.py
@@ -5,18 +5,18 @@ from datetime import datetime, timezone
 def min_unit(diff):
     if diff < 10:
         return "seconds"
-    if diff < 3600:
+    if diff < 360:
         return "minutes"
-    if diff < 7200:
+    if diff < 720:
         return "hours"
     return "days"
 
 
-def get_time_ago(past_date: datetime):
+def get_time_ago(past_date):
 
     humanize.i18n.activate("es_ES")
     now = datetime.now(timezone.utc)
-    diff = now - past_date
+    diff = past_date
     second_diff = diff.seconds
     delta = humanize.precisedelta(
         past_date - now, minimum_unit=min_unit(second_diff), format="%d")
